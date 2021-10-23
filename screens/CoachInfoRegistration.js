6b9661469e128b1337c088788107c85b801ff791
Victoria Brennick <40518417+toricastle@users.noreply.github.com>
2021-10-23 09:50:39 -0400

    added new feautures

--- screens/CoachInfoRegistration.js
+++ screens/CoachInfoRegistration.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text, TextInput} from "react-native";
 
 const CoachInfoRegistration = ({ navigation }) => {
   return (
     <View>
-      <Text>Text!!!!!!!!!!!!!!</Text>
+      
+
+      <TextInput style={styles.jobInput}
+        onChangeText={setJobTitle}
+        value={jobTitle}
+        placeholder="Enter your Job Title"></TextInput>
 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  
+    jobInput: {
+        width: 237,
+        height: 40,
+        borderWidth: 1,
+        borderColor: "black",
+        borderRadius: 8,
+        padding: 10,
+        marginTop: 100,
+      },
+
+  });
 
 export default CoachInfoRegistration;
