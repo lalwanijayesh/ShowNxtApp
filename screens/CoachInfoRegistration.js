9887930eb3fa44d5e0967995b9d3a296b5f21d58
Nick Salesky <nicksalesky@gmail.com>
2021-10-23 11:12:24 -0400

    Made CoachInfoRegistration a class component

--- screens/CoachInfoRegistration.js
+++ screens/CoachInfoRegistration.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { View, Text, TextInput} from "react-native";
+import { View, Text, TextInput } from "react-native";
 
-const CoachInfoRegistration = ({ navigation }) => {
-  return (
-    <View>
-      
+class CoachInfoRegistration extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-      <TextInput style={styles.jobInput}
-        onChangeText={setJobTitle}
-        value={jobTitle}
-        placeholder="Enter your Job Title"></TextInput>
-
-  
-  </View>);
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={styles.jobInput}
+          onChangeText={setJobTitle}
+          value={jobTitle}
+          placeholder="Enter your Job Title"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  
-    jobInput: {
-        width: 237,
-        height: 40,
-        borderWidth: 1,
-        borderColor: "black",
-        borderRadius: 8,
-        padding: 10,
-        marginTop: 100,
-      },
-
-  });
+  jobInput: {
+    width: 237,
+    height: 40,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 8,
+    padding: 10,
+    marginTop: 100,
+  },
+});
 
 export default CoachInfoRegistration;
