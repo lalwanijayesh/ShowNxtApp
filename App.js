27cd4090eb19fbd1454bbb1a08e7371875c9ebba
Ngoc (Victoria) Le <58549806+khanhvy1703@users.noreply.github.com>
2021-10-25 13:40:14 -0400

    remove comments
    Co-authored-by: Ameya Bhat <ameyabhat1918@gmail.com>

--- App.js
+++ App.js
@@ -40,7 +40,6 @@ export default function App() {
         />
       </Stack.Navigator>
 
-      {/* <Stack.Navigator>
         <Stack.Screen
           name="Welcome"
           component={WelcomeScreen}
