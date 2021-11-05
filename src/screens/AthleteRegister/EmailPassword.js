c93d4f1468522e61d19bdc26b65599504b6904f0
Ngoc (Victoria) Le <58549806+khanhvy1703@users.noreply.github.com>
2021-11-05 00:33:12 -0400

    merge conflict and the bug in email and password fix

--- src/screens/AthleteRegister/EmailPassword.js
+++ src/screens/AthleteRegister/EmailPassword.js
@@ -10,7 +10,7 @@ for (let i = 0; i < symbols.length; i++) {
 }
 
 // numbers array
-const numArray = ['1','2','3','4','5','6','7','8','9'];
+const numArray = ['0','1','2','3','4','5','6','7','8','9'];
 
 // TODO: Firebase to store and auth email
 const EmailPassword = ({navigation}) => {
