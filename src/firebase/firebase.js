89d46ea8dd8c21326de1bfae3aed9dac07131df5
Ngoc (Victoria) Le <58549806+khanhvy1703@users.noreply.github.com>
2021-11-18 02:17:34 -0500

    hide firebase API key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,20 +2,13 @@ import firebase from "firebase";
 import { Alert } from "react-native";
 
 const firebaseConfig = {
-  // apiKey: "EnterYourApiKeyHere",
-  // authDomain: "AuthDomain",
-  // projectId: "ProjectId",
-  // storageBucket: "StorageBucket",
-  // messagingSenderId: "MessagingSenderId",
-  // appId: "AppId",
-  // measurementId: "MeasurementId"
-  apiKey: "AIzaSyCdiT_N4bQEKdKV31nB4M2I1e9_Ogm4yRs",
-  authDomain: "generate-shownxt.firebaseapp.com",
-  projectId: "generate-shownxt",
-  storageBucket: "generate-shownxt.appspot.com",
-  messagingSenderId: "1020751547045",
-  appId: "1:1020751547045:web:4ca38ad36f616639479b4c",
-  measurementId: "G-573TD7VK4V"
+  apiKey: "EnterYourApiKeyHere",
+  authDomain: "AuthDomain",
+  projectId: "ProjectId",
+  storageBucket: "StorageBucket",
+  messagingSenderId: "MessagingSenderId",
+  appId: "AppId",
+  measurementId: "MeasurementId"
 };
 
 
