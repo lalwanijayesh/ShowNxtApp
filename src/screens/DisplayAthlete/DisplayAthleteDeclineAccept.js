0ced6da740d0f025d97ec6e9126e0323f79729d0
Victoria Brennick <40518417+toricastle@users.noreply.github.com>
2021-11-29 16:06:53 -0500

    updated displaying athlete to coach

--- src/screens/DisplayAthlete/DisplayAthleteDeclineAccept.js
+++ src/screens/DisplayAthlete/DisplayAthleteDeclineAccept.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import {
   View,
   ScrollView,
@@ -7,27 +7,40 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
+  Button,
+  Platform,
 } from "react-native";
-import Video from "react-native-video";
 import Icon from "react-native-ico-material-design";
+import { Video } from "expo-av";
+import * as ImagePicker from "expo-image-picker";
+import sampleVideo from "../../../assets/video/sample.mp4";
+import sampleVideoDog from "../../../assets/video/dogvid.mp4";
 
 // TODO: make a method that for each video displays a little white circle at the bottom of the screen
 
 const DisplayAthlete = ({ navigation }) => {
+  const videoRef = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    // <View style={{ alignItems: "center", flex: 1, justifyContent: "center" }}>
-    //   <View>
-    //     <Video
-    //       source={{
-    //         uri: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4?_=1",
-    //       }}
-    //       repeat={true}
-    //       paused={false}
-    //     />
-    //   </View>
-    // </View>
-
     <View style={styles.container}>
+      {/* DISPLAYING THE VIDEO  */}
+      <View style={styles.containerVid}>
+        {sampleVideo && (
+          <Video
+            ref={videoRef}
+            style={styles.video}
+            // source={{uri: video}}
+            source={sampleVideoDog}
+            useNativeControls
+            isLooping
+            resizeMode="contain"
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+          />
+        )}
+      </View>
+      {/* DISPLAYING THE VIDEO  */}
+
+      {/* NAVIGATION BAR ON THE BOTTOM OF PAGE */}
       <View style={styles.navContainer}>
         <View style={styles.navBar}>
           <TouchableOpacity
@@ -63,6 +76,9 @@ const DisplayAthlete = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       </View>
+      {/* NAVIGATION BAR ON THE BOTTOM OF PAGE */}
+
+      {/* BUTTONS TO REJECT AND ACCEPT + DOTS FOR EACH VID */}
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonsBar}>
           <TouchableOpacity style={styles.circleDecline}>
@@ -74,11 +90,42 @@ const DisplayAthlete = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       </View>
+      {/* BUTTONS TO REJECT AND ACCEPT + DOTS FOR EACH VID */}
+
+      {/* NAME/AGE OF ATHLETE */}
+      <View style={styles.nameAgeBar}>
+        <Text style={styles.textName}> Jake Smith, 19</Text>
+      </View>
+      {/* NAME/AGE OF ATHLETE */}
+
+      {/* POSITION OF ATHLETE */}
+      <View style={styles.positionBar}>
+        <Text style={styles.textLocation}> 🏐 Goalkeeper</Text>
+      </View>
+      {/* POSITION OF ATHLETE */}
+
+      {/* LOCATION OF ATHLETE */}
+      <View style={styles.locationBar}>
+        <Text style={styles.textLocation}> 📍 Boston, MA</Text>
+      </View>
+      {/* LOCATION OF ATHLETE */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  containerVid: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  video: {
+    alignSelf: "center",
+    //width: 640 * 1.3,
+    //height: 400 * 1.3,
+    width: 640 * 2.3,
+    height: 400 * 2.3,
+  },
   container: {
     alignItems: "center",
     flex: 1,
@@ -102,7 +149,7 @@ const styles = StyleSheet.create({
   buttonsBar: {
     flexDirection: "row",
     width: "100%",
-    backgroundColor: `#000000`,
+    // backgroundColor: `#000000`,
     alignItems: "center",
     justifyContent: "space-evenly",
   },
@@ -113,22 +160,54 @@ const styles = StyleSheet.create({
     color: `#000000`,
   },
 
+  locationBar: {
+    flexDirection: "row",
+    width: "90%",
+    //backgroundColor: `pink`,
+    //alignItems: "center",
+    justifyContent: "flex-start",
+    position: "absolute",
+    bottom: 190,
+    alignItems: "flex-start",
+  },
+  positionBar: {
+    flexDirection: "row",
+    width: "90%",
+    //backgroundColor: `pink`,
+    //alignItems: "center",
+    justifyContent: "flex-start",
+    position: "absolute",
+    bottom: 220,
+    alignItems: "flex-start",
+  },
+
+  nameAgeBar: {
+    flexDirection: "row",
+    width: "90%",
+    //backgroundColor: `pink`,
+    //alignItems: "center",
+    justifyContent: "flex-start",
+    position: "absolute",
+    bottom: 253,
+    alignItems: "flex-start",
+  },
+
   icon: {
     padding: 14,
   },
   circleDecline: {
-    width: 70,
-    height: 70,
-    borderRadius: 70 / 2,
+    width: 80,
+    height: 80,
+    borderRadius: 80 / 2,
     backgroundColor: "red",
     margin: 5,
     display: "flex",
     alignItems: "center",
   },
   circleAccept: {
-    width: 70,
-    height: 70,
-    borderRadius: 70 / 2,
+    width: 80,
+    height: 80,
+    borderRadius: 80 / 2,
     backgroundColor: "mediumseagreen",
     margin: 5,
     display: "flex",
@@ -148,7 +227,18 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 40,
     textAlign: "center",
-    margin: 11,
+    margin: 15,
+  },
+  textLocation: {
+    // fontWeight: "bold",
+    color: "black",
+    fontSize: 25,
+  },
+
+  textName: {
+    fontWeight: "bold",
+    color: "black",
+    fontSize: 35,
   },
 });
 
