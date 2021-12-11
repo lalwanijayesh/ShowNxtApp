2c473784adcbc3806f22a6433079effd739a6d4a
Nick Salesky <nicksalesky@gmail.com>
2021-12-11 11:13:47 -0500

    Fixed a few errors

--- src/screens/AthleteProfile/CompleteAthleteProfile.js
+++ src/screens/AthleteProfile/CompleteAthleteProfile.js
@@ -11,10 +11,12 @@ import ScreenNames from "../../constants/ScreenNames";
 // TODO: Replace image in the circle view
 // TODO: find the way to let the user upload videos into react-native
 // TODO: This screen is currently a pure UI, it will be fixed later to use to connect back-end for user data.
-const AthleteProfile = ({ navigation }) => {
-  <View>
-    <Text> Hello </Text>
-  </View>;
+const CompleteAthleteProfile = ({ navigation }) => {
+  return (
+    <View>
+      <Text> Hello </Text>
+    </View>
+  );
 };
 
 export default CompleteAthleteProfile;
