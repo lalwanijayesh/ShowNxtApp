3154ba1efac45e0e7d7cfca214a9cdcb16db9f46
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2021-12-20 23:35:13 +0530

    Restructure video upload layout and dynamic video list

--- src/screens/AthleteRegister/AthleteComplete.js
+++ src/screens/AthleteRegister/AthleteComplete.js
@@ -4,12 +4,17 @@ import {
   View,
   Text,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  Platform,
+  ActivityIndicator
 } from "react-native";
 import ScreenNames from "../../constants/ScreenNames";
-import ImagePickerExample from "../VideoUpload";
 import UserIdContext from "../../AppContext";
 import {gql, useMutation} from "@apollo/client";
+import * as ImagePicker from "expo-image-picker";
+import firebase from "../../firebase/firebase";
+import uuid from "react-native-uuid";
+import {Video} from "expo-av";
 
 const ADD_VIDEO = gql`
 mutation AddProfileVideo($profileId: ID!, $filePath: String!) {
@@ -26,32 +31,101 @@ const AthleteComplete = ({ navigation, route }) => {
   const [addVideo] = useMutation(ADD_VIDEO, {
     onError: (error) => console.log(error),
   });
+  const [loading, setLoading] = useState(false);
 
-  const testData = [
-    {
-      id: "1",
-    },
-    {
-      id: "2",
-    },
-    {
-      id: "3",
-    },
-    {
-      id: "4",
-    },
-  ];
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
 
-  const renderItem = (item) => {
+  const [videos, setVideos] = useState([
+    {
+      id: 0,
+      source: null
+    }
+  ]);
+
+  const pickVideo = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      console.log(result.uri);
+      await uploadVideo(result.uri);
+    }
+  };
+
+  const uploadVideo = async (uri) => {
+    setLoading(true);
+    const storageRef = firebase.storage().ref();
+    const filePath = "videos/" + uuid.v4()
+        + uri.substring(uri.lastIndexOf("."));
+    const fileRef = storageRef.child(filePath);
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+
+    fileRef.put(blob).then((snapshot) => {
+      console.log('Uploaded video successfully!');
+      blob.close();
+      addVideo({
+        variables: {
+          profileId: route.params.profileId,
+          filePath: filePath,
+        },
+      }).then(res => {
+        console.log("File Path saved - " + filePath);
+        setVideos(videos => [...videos, {
+          id: res.data.addProfileVideo.videoId,
+          source: uri
+        }]);
+        setLoading(false);
+      });
+    });
+  }
+
+  const renderItem = ({item}) => {
     return (
-      <View style={{ marginRight: 17 }}>
-        <View style={styles.videoStyle}>
-          <ImagePickerExample
-              profileId={route.params.profileId}
-              addVideo={addVideo}
-          />
+        <View style={{ marginRight: 17 }}>
+          <View style={styles.videoStyle}>
+            {item.id === 0 ? (
+            <View style={styles.buttons}>
+              <TouchableOpacity onPress={pickVideo}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{"+"}</Text>
+              </TouchableOpacity>
+            </View>
+            ) : (
+            <Video
+                style={styles.video}
+                source={{ uri: item.source }}
+                useNativeControls={false}
+                resizeMode="cover"
+                isLooping
+                shouldPlay
+            />
+            )}
+          </View>
         </View>
-      </View>
     );
   };
 
@@ -66,7 +140,9 @@ const AthleteComplete = ({ navigation, route }) => {
       <Text style={styles.register}>{"COMPLETE PROFILE"}</Text>
 
       <View style={styles.infoContainer}>
-        <View style={styles.avatar}></View>
+        <View style={styles.avatar}>
+          {/* TODO add profile avatar here */}
+        </View>
         <View>
           <Text style={styles.textName}>{route.params.fullName}</Text>
           <Text style={[styles.spacingBetweenText, styles.textInfo]}>
@@ -80,11 +156,11 @@ const AthleteComplete = ({ navigation, route }) => {
       <View style={{ marginHorizontal: 34, marginTop: 23 }}>
         <FlatList
           bounces={false}
-          data={testData}
+          data={videos}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => renderItem(item)}
-          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index}
         />
       </View>
 
@@ -96,12 +172,17 @@ const AthleteComplete = ({ navigation, route }) => {
         style={styles.nextBtn}
         onPress={() => {
           setUserId(route.params.userId);
-
           navigation.navigate(ScreenNames.ATHLETE_TAB_FLOW, route.params);
         }}
       >
         <Text style={styles.nextText}>{"Start Exploring"}</Text>
       </TouchableOpacity>
+      {loading &&
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" color="#0000FF" />
+        <Text>Uploading ...</Text>
+      </View>
+      }
     </View>
   );
 };
@@ -211,4 +292,27 @@ const styles = StyleSheet.create({
     fontSize: 14,
     lineHeight: 16,
   },
+
+  video: {
+    width: 135,
+    height: 271,
+    borderRadius: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttons: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    position: 'absolute',
+    backgroundColor: '#F5FCFF88',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
